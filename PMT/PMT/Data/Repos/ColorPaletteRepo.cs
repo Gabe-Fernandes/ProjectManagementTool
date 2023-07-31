b9b74a4420dfcdaec012e1667dff6baf7c076b85
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2023-07-31 10:05:50 -0400

    7/31/23 ColorPaletteRepo Interface created but bodies of methods not implemented. ColorPaletteRepo unit tests written and intentionally fail for TDD

--- PMT/PMT/Data/Repos/ColorPaletteRepo.cs
+++ PMT/PMT/Data/Repos/ColorPaletteRepo.cs
@@ -1,11 +1,38 @@
-﻿namespace PMT.Data.Models;
+﻿using PMT.Data.RepoInterfaces;
 
-public class ColorPaletteRepo
+namespace PMT.Data.Models;
+
+public class ColorPaletteRepo : IColorPaletteRepo
 {
   private readonly AppDbContext _db;
 
   public ColorPaletteRepo(AppDbContext db)
   {
     _db = db;
+  }
+
+  public bool Add(ColorPalette colorPalette)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Delete(ColorPalette colorPalette)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<ColorPalette> GetByProjectIdAsync(int projId)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Save()
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Update(ColorPalette colorPalette)
+  {
+    throw new NotImplementedException();
   }
 }

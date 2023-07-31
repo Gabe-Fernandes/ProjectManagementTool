ffcdfb55fb9f7e88ebc24518f5d93fc224097f85
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2023-07-31 09:01:55 -0400

    7/31/23 BugReportRepo Interface created but bodies of methods not implemented. BugReportRepo unit tests written and intentionally fail for TDD

--- PMT/PMT/Data/RepoInterfaces/IBugReportRepo.cs
+++ PMT/PMT/Data/RepoInterfaces/IBugReportRepo.cs
@@ -1,5 +1,16 @@
-﻿namespace PMT.Data.RepoInterfaces;
+﻿using PMT.Data.Models;
+
+namespace PMT.Data.RepoInterfaces;
 
 public interface IBugReportRepo
 {
+  Task<IEnumerable<BugReport>> GetAllAsync(int projId);
+  Task<IEnumerable<BugReport>> GetAllUnresolvedReportsAsync(int projId);
+  Task<IEnumerable<BugReport>> GetAllFromUserAsync(int projId, string appUserId);
+  Task<IEnumerable<BugReport>> GetAllUnresolvedReportsFromUserAsync(int projId, string appUserId);
+  Task<BugReport> GetByIdAsync(int id);
+  bool Add(BugReport appUser);
+  bool Update(BugReport appUser);
+  bool Delete(BugReport appUser);
+  bool Save();
 }
--- PMT/PMT/Data/Repos/BugReportRepo.cs
+++ PMT/PMT/Data/Repos/BugReportRepo.cs
@@ -1,11 +1,58 @@
-﻿namespace PMT.Data.Models;
+﻿using PMT.Data.RepoInterfaces;
 
-public class BugReportRepo
+namespace PMT.Data.Models;
+
+public class BugReportRepo : IBugReportRepo
 {
   private readonly AppDbContext _db;
 
   public BugReportRepo(AppDbContext db)
   {
     _db = db;
+  }
+
+  public bool Add(BugReport appUser)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Delete(BugReport appUser)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<BugReport>> GetAllAsync(int projId)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<BugReport>> GetAllFromUserAsync(int projId, string appUserId)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<BugReport>> GetAllUnresolvedReportsAsync(int projId)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<BugReport>> GetAllUnresolvedReportsFromUserAsync(int projId, string appUserId)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<BugReport> GetByIdAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Save()
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Update(BugReport appUser)
+  {
+    throw new NotImplementedException();
   }
 }

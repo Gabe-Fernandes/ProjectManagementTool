f8bbd55982104ffec509daa1295efcfd3d4414b9
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2023-08-01 11:14:21 -0400

    8/1/23 ProjectRepo Interface created but bodies of methods not implemented. ProjectRepo unit tests written and intentionally fail for TDD

--- PMT/PMT/Data/Repos/ProjectRepo.cs
+++ PMT/PMT/Data/Repos/ProjectRepo.cs
@@ -1,11 +1,43 @@
-﻿namespace PMT.Data.Models;
+﻿using PMT.Data.RepoInterfaces;
 
-public class ProjectRepo
+namespace PMT.Data.Models;
+
+public class ProjectRepo : IProjectRepo
 {
   private readonly AppDbContext _db;
 
   public ProjectRepo(AppDbContext db)
   {
     _db = db;
+  }
+
+  public bool Add(Project project)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Delete(Project project)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<IEnumerable<Project>> GetAllFromUserAsync(string appUserId)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<Project> GetByIdAsync(int id)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Save()
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Update(Project project)
+  {
+    throw new NotImplementedException();
   }
 }
--- PMT/PMTTests/Data/Repos/ProjectRepoTests.cs
+++ PMT/PMTTests/Data/Repos/ProjectRepoTests.cs
@@ -1,10 +1,132 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using PMT.Data.Models;
+using PMT.Data;
 
 namespace PMTTests.Data.Repos;
-internal class ProjectRepoTests
+
+public class ProjectRepoTests
 {
+  private readonly AppDbContext _dbContext;
+  private readonly ProjectRepo _projectRepo;
+
+  public ProjectRepoTests()
+  {
+    // Dependencies
+    _dbContext = GetDbContext();
+    // SUT
+    _projectRepo = new ProjectRepo(_dbContext);
+  }
+
+  private AppDbContext GetDbContext()
+  {
+    var options = new DbContextOptionsBuilder<AppDbContext>()
+      .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+      .Options;
+    var dbContext = new AppDbContext(options);
+    dbContext.Database.EnsureCreated();
+    if (dbContext.Projects.Count() < 0)
+    {
+      for (int i = 0; i < 10; i++)
+      {
+        dbContext.Projects.Add(new Project()
+        {
+          Id = (i + 1),
+          StartDate = DateTime.Now,
+          DueDate = DateTime.Now,
+          JoinCode = "test string",
+          Name = "test string"
+        });
+        dbContext.SaveChangesAsync();
+      }
+    }
+    return dbContext;
+  }
+
+  [Fact]
+  public void Add_ReturnsTrue()
+  {
+    // Arrange
+    var project = new Project()
+    {
+      Id = 2,
+      StartDate = DateTime.Now,
+      DueDate = DateTime.Now,
+      JoinCode = "test string",
+      Name = "test string"
+    };
+    // Act
+    var result = _projectRepo.Add(project);
+    // Assert
+    Assert.True(result);
+  }
+
+  [Fact]
+  public void Delete_ReturnsTrue()
+  {
+    // Arrange
+    var project = new Project()
+    {
+      Id = 2,
+      StartDate = DateTime.Now,
+      DueDate = DateTime.Now,
+      JoinCode = "test string",
+      Name = "test string"
+    };
+    _projectRepo.Add(project);
+    // Act
+    var result = _projectRepo.Delete(project);
+    // Assert
+    Assert.True(result);
+  }
+
+  [Fact]
+  public void Update_ReturnsTrue()
+  {
+    // Arrange
+    var project = new Project()
+    {
+      Id = 2,
+      StartDate = DateTime.Now,
+      DueDate = DateTime.Now,
+      JoinCode = "test string",
+      Name = "test string"
+    };
+    _projectRepo.Add(project);
+    // Act
+    var result = _projectRepo.Update(project);
+    // Assert
+    Assert.True(result);
+  }
+
+  [Fact]
+  public void Save_ReturnsBool()
+  {
+    // Arrange (empty)
+    // Act
+    var result = _projectRepo.Save();
+    // Assert
+    Assert.IsType<bool>(result);
+  }
+
+  [Fact]
+  public async void GetByIdAsync_ReturnsProjectTask()
+  {
+    // Arrange
+    var id = 2;
+    // Act
+    var result = _projectRepo.GetByIdAsync(id);
+    // Assert
+    await Assert.IsType<Task<Project>>(result);
+  }
+
+  [Fact]
+  public async void GetAllFromUserAsync_ReturnsIEnumerableProjectTask()
+  {
+    // Arrange
+    string appUserId = "test";
+    // Act
+    var result = _projectRepo.GetAllFromUserAsync(appUserId);
+    // Assert
+    await Assert.IsType<Task<IEnumerable<Project>>>(result);
+  }
 }

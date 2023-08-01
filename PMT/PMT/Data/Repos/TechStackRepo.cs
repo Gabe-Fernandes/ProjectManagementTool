b6d106b127e64d92e245f4c918fb10de3d24e150
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2023-08-01 10:53:42 -0400

    8/1/23 TechStackRepo Interface created but bodies of methods not implemented. TechStackRepo unit tests written and intentionally fail for TDD

--- PMT/PMT/Data/Repos/TechStackRepo.cs
+++ PMT/PMT/Data/Repos/TechStackRepo.cs
@@ -1,11 +1,38 @@
-﻿namespace PMT.Data.Models;
+﻿using PMT.Data.RepoInterfaces;
 
-public class TechStackRepo
+namespace PMT.Data.Models;
+
+public class TechStackRepo : ITechStackRepo
 {
   private readonly AppDbContext _db;
 
   public TechStackRepo(AppDbContext db)
   {
     _db = db;
+  }
+
+  public bool Add(TechStack techStack)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Delete(TechStack techStack)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<TechStack> GetByProjectIdAsync(int projId)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Save()
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Update(TechStack techStack)
+  {
+    throw new NotImplementedException();
   }
 }

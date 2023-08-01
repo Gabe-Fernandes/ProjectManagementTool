e26a98b89bdbc1a6de413349e00af3971b86b768
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2023-08-01 11:04:47 -0400

    8/1/23 SRSRepo Interface created but bodies of methods not implemented. SRSRepo unit tests written and intentionally fail for TDD

--- PMT/PMT/Data/Repos/SRSRepo.cs
+++ PMT/PMT/Data/Repos/SRSRepo.cs
@@ -1,11 +1,38 @@
-﻿namespace PMT.Data.Models;
+﻿using PMT.Data.RepoInterfaces;
 
-public class SRSRepo
+namespace PMT.Data.Models;
+
+public class SRSRepo : ISRSRepo
 {
   private readonly AppDbContext _db;
 
   public SRSRepo(AppDbContext db)
   {
     _db = db;
+  }
+
+  public bool Add(SRS SRS)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Delete(SRS SRS)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<SRS> GetByProjectIdAsync(int projId)
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Save()
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool Update(SRS SRS)
+  {
+    throw new NotImplementedException();
   }
 }
--- PMT/PMTTests/Data/Repos/SRSRepoTests.cs
+++ PMT/PMTTests/Data/Repos/SRSRepoTests.cs
@@ -1,10 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using PMT.Data.Models;
+using PMT.Data;
 
 namespace PMTTests.Data.Repos;
-internal class SRSRepoTests
+
+public class SRSRepoTests
 {
+  private readonly AppDbContext _dbContext;
+  private readonly SRSRepo _SRSRepo;
+
+  public SRSRepoTests()
+  {
+    // Dependencies
+    _dbContext = GetDbContext();
+    // SUT
+    _SRSRepo = new SRSRepo(_dbContext);
+  }
+
+  private AppDbContext GetDbContext()
+  {
+    var options = new DbContextOptionsBuilder<AppDbContext>()
+      .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+      .Options;
+    var dbContext = new AppDbContext(options);
+    dbContext.Database.EnsureCreated();
+    if (dbContext.SRSs.Count() < 0)
+    {
+      for (int i = 0; i < 10; i++)
+      {
+        dbContext.SRSs.Add(new SRS()
+        {
+          Id = (i + 1),
+          ProjId = i + 2,
+          DomainName = "test string",
+          TargetDemographic = "test string",
+          Description = "test string"
+        });
+        dbContext.SaveChangesAsync();
+      }
+    }
+    return dbContext;
+  }
+
+  [Fact]
+  public void Add_ReturnsTrue()
+  {
+    // Arrange
+    var SRS = new SRS()
+    {
+      Id = 2,
+      ProjId = 3,
+      DomainName = "test string",
+      TargetDemographic = "test string",
+      Description = "test string"
+    };
+    // Act
+    var result = _SRSRepo.Add(SRS);
+    // Assert
+    Assert.True(result);
+  }
+
+  [Fact]
+  public void Delete_ReturnsTrue()
+  {
+    // Arrange
+    var SRS = new SRS()
+    {
+      Id = 2,
+      ProjId = 3,
+      DomainName = "test string",
+      TargetDemographic = "test string",
+      Description = "test string"
+    };
+    _SRSRepo.Add(SRS);
+    // Act
+    var result = _SRSRepo.Delete(SRS);
+    // Assert
+    Assert.True(result);
+  }
+
+  [Fact]
+  public void Update_ReturnsTrue()
+  {
+    // Arrange
+    var SRS = new SRS()
+    {
+      Id = 2,
+      ProjId = 3,
+      DomainName = "test string",
+      TargetDemographic = "test string",
+      Description = "test string"
+    };
+    _SRSRepo.Add(SRS);
+    // Act
+    var result = _SRSRepo.Update(SRS);
+    // Assert
+    Assert.True(result);
+  }
+
+  [Fact]
+  public void Save_ReturnsBool()
+  {
+    // Arrange (empty)
+    // Act
+    var result = _SRSRepo.Save();
+    // Assert
+    Assert.IsType<bool>(result);
+  }
+
+  [Fact]
+  public async void GetByProjectIdAsync_ReturnsSRSTask()
+  {
+    // Arrange
+    int projId = 3;
+    // Act
+    var result = _SRSRepo.GetByProjectIdAsync(projId);
+    // Assert
+    await Assert.IsType<Task<SRS>>(result);
+  }
 }

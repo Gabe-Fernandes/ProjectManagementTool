14418746b8b45aca0741f047b049b8c609296267
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2023-08-24 15:34:30 -0400

    8/24/23 Project Frontend: MyProjects, DeleteProject, and ProjectDash - html, scss, and js. Controllers built enough to route get methods.

--- PMT/PMT/wwwroot/js/site.js
+++ PMT/PMT/wwwroot/js/site.js
@@ -94,18 +94,18 @@ function chronologicallyFirst(date1, date2) {
 }
 function abbreviatedMonthToInt(abbreviatedMonth) {
   switch (abbreviatedMonth) {
-    case "Jan": return 1; break;
-    case "Feb": return 2; break;
-    case "Mar": return 3; break;
-    case "Apr": return 4; break;
-    case "May": return 5; break;
-    case "Jun": return 6; break;
-    case "Jul": return 7; break;
-    case "Aug": return 8; break;
-    case "Sep": return 9; break;
-    case "Oct": return 10; break;
-    case "Nov": return 11; break;
-    case "Dec": return 12; break;
+    case "Jan": return 1;
+    case "Feb": return 2;
+    case "Mar": return 3;
+    case "Apr": return 4;
+    case "May": return 5;
+    case "Jun": return 6;
+    case "Jul": return 7;
+    case "Aug": return 8;
+    case "Sep": return 9;
+    case "Oct": return 10;
+    case "Nov": return 11;
+    case "Dec": return 12;
   }
 }
 

be4caa5746e83264067cae703773cd1700579044
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2023-09-05 12:52:20 -0400

    9/5/23 Added client-side validation for the login view

--- PMT/PMT/wwwroot/js/Account/login.js
+++ PMT/PMT/wwwroot/js/Account/login.js
@@ -5,17 +5,16 @@
   // check for failed login on page load
   const invalidCredentialErrMsg = "Email or password are incorrect";
   if ($(".failed-login").length === 1) {
-    // Error ID html not written yet
-    //ShowError("loginEmail", "loginEmailErr", invalidCredentialErrMsg);
-    //ShowError("loginPassword", "loginPasswordErr", invalidCredentialErrMsg);
+    ShowError("loginEmail", "loginEmailErr", invalidCredentialErrMsg);
+    ShowError("loginPass", "loginPassErr", invalidCredentialErrMsg);
   }
 
   $("#loginEmail").on("input", clearLoginErrosAfterLoginFail);
   $("#loginPassword").on("input", clearLoginErrosAfterLoginFail);
   function clearLoginErrosAfterLoginFail() {
-    if ($("#loginPasswordErr")[0].innerHTML === invalidCredentialErrMsg) {
+    if ($("#loginPassErr")[0].innerHTML === invalidCredentialErrMsg) {
       HideError("loginEmail", "loginEmailErr");
-      HideError("loginPassword", "loginPasswordErr");
+      HideError("loginPass", "loginPassErr");
     }
   }
 
@@ -59,5 +58,73 @@
   }
   if ($("#resendEmailConfForm").length === 0) {
     ToggleModal($("#loginContent"), $("#resendEmailConfModal"), openModal);
+  }
+
+  // validation
+
+  const charLimit = 40;
+  const allInputNames = ["Email", "Pass", "ForgotPassEmail", "ResendEmailConfEmail"];
+  let allInputIDs = [];
+  let allInputFields = [];
+  let allErrIDs = [];
+
+  for (let i = 0; i < allInputNames.length; i++) {
+    allInputIDs.push(`login${allInputNames[i]}`);
+    allInputFields.push($(`#${allInputIDs[i]}`));
+    allErrIDs.push(`${allInputIDs[i]}Err`);
+  }
+
+  $(".login-form").on("submit", (evt) => {
+    const inputFields = [$("#loginEmail"), $("#loginPass")];
+    const errIDs = ["loginEmailErr", "loginPassErr"];
+
+    RunCommonValidationTests(inputFields, errIDs, charLimit);
+
+    // test valid email
+    if (ValidEmail($("#loginEmail").val()) === false) {
+      ShowError("loginEmail", "loginEmailErr", "invalid email");
+    }
+
+    if ($(".err-input").length > 0) { evt.preventDefault() }
+  });
+
+  $("#recoverPassForm").on("submit", (evt) => {
+    const inputFields = $("#loginForgotPassEmail");
+    const errIDs = "loginForgotPassEmailErr";
+
+    RunCommonValidationTests(inputFields, errIDs, charLimit);
+
+    // test valid email
+    if (ValidEmail($("#loginForgotPassEmail").val()) === false) {
+      ShowError("loginForgotPassEmail", "loginForgotPassEmailErr", "invalid email");
+    }
+
+    if ($(".err-input").length > 0) { evt.preventDefault() }
+  });
+
+  $("#resendEmailConfForm").on("submit", (evt) => {
+    const inputFields = $("#loginResendEmailConfEmail");
+    const errIDs = "loginResendEmailConfEmailErr";
+
+    RunCommonValidationTests(inputFields, errIDs, charLimit);
+
+    // test valid email
+    if (ValidEmail($("#loginResendEmailConfEmail").val()) === false) {
+      ShowError("loginResendEmailConfEmail", "loginResendEmailConfEmailErr", "invalid email");
+    }
+
+    if ($(".err-input").length > 0) { evt.preventDefault() }
+  });
+
+  // Real-Time Validation
+  realTimeValidation(allInputIDs, allErrIDs, allInputFields, charLimit, loginValidations);
+
+  function loginValidations(i) {
+    if (allInputIDs[i] === "loginEmail" || allInputIDs[i] === "loginForgotPassEmail" || allInputIDs[i] === "loginResendEmailConfEmail") {
+      if (ValidEmail(allInputFields[i].val()) === false) {
+        ShowError(allInputIDs[i], allErrIDs[i], "invalid email");
+        return true; // errorExists === true
+      }
+    }
   }
 });

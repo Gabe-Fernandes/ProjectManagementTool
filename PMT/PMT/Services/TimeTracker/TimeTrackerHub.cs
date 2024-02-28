c3ae86d6581ebb60a3d8210d4a96d3ee7627d45a
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2024-02-28 18:24:24 -0500

    2/28/24 setup signalR and worked on more frontend dev

--- PMT/PMT/Services/TimeTracker/TimeTrackerHub.cs
+++ PMT/PMT/Services/TimeTracker/TimeTrackerHub.cs
@@ -1,5 +1,15 @@
-﻿namespace PMT.Services.TimeTracker;
+﻿using Microsoft.AspNetCore.SignalR;
+using PMT.Data.RepoInterfaces;
 
-public class TimeTrackerHub
+namespace PMT.Services.TimeTracker;
+
+public class TimeTrackerHub(IStopwatchRepo stopwatchRepo,
+	IShiftRepo shiftRepo) : Hub<ITimeTrackerHub>
 {
+	private readonly IStopwatchRepo _stopwatchRepo = stopwatchRepo;
+	private readonly IShiftRepo _shiftRepo = shiftRepo;
+
+
+
+
 }

cef7600e21770cb00c430a835401f7a5709ddaa9
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2024-04-02 11:21:40 -0400

    4/2/24 table pagination frontend mostly done

--- PMT/PMT/wwwroot/js/Agile/myStories.js
+++ PMT/PMT/wwwroot/js/Agile/myStories.js
@@ -45,6 +45,7 @@
     const color0 = $(".table-wrap").find("tbody:first").find("tr:first").css("background-color");
     const color1 = $(".table-wrap").css("background-color");
     let toggle = 0;
+    let foundSet = []; // create array for tr's
 
     for (let i = 0; i < $(".sortStatus").length; i++) { // iterate through each tr
       const status = $(".sortStatus").eq(i).find("label").html();
@@ -58,6 +59,7 @@
 
       if (title.includes(filterString)) {
         trElement.removeClass("hide");
+        foundSet.push(trElement); // used in table pagination
         // force the striped pattern because hidden tr's disrupt this
         const colorToUse = (toggle === 0) ? color0 : color1;
         trElement.css("background-color", colorToUse);
@@ -67,9 +69,93 @@
         trElement.addClass("hide");
       }
     }
+    paginateTable(foundSet);
   }
 
   $("#filterInput").on("input", searchFilter);
   $("#showResolvedCheckbox").on("input", searchFilter);
+
+
+
+  // table pagination
+  let currentPage = 1;
+  let resultsPerPage = 10;
+  let numOfPages = 0;
+  let pages = [];
+
+  $("#firstPageBtn").on("click", () => {
+    currentPage = 1;
+    $("#currentPageInput").val(currentPage);
+  });
+  $("#prevPageBtn").on("click", () => {
+    if (currentPage > 1) {
+      currentPage--;
+      $("#currentPageInput").val(currentPage);
+    }
+  });
+  $("#nextPageBtn").on("click", () => {
+    if (currentPage < numOfPages) {
+      currentPage++;
+      $("#currentPageInput").val(currentPage);
+    }
+  });
+  $("#lastPageBtn").on("click", () => {
+    currentPage = numOfPages;
+    $("#currentPageInput").val(currentPage);
+  });
+
+
+
+  $("#resultsPerPageInput").on("input", () => {
+    // clear validation
+    HideError("resultsPerPageInput", "resultsPerPageInputErr");
+
+    // validate input is a positive integer
+    const input = $("#resultsPerPageInput").val();
+
+    for (let i = 0; i < input.length; i++) {
+      if (NumericalChars.includes(input[i]) === false || input <= 0) {
+        ShowError("resultsPerPageInput", "resultsPerPageInputErr", "Invalid entry");
+        return;
+      }
+    }
+    resultsPerPage = parseInt(input);
+  });
+
+  $("#currentPageInput").on("input", () => {
+    // clear validation
+    HideError("currentPageInput", "currentPageInputErr");
+
+    // validate input is an integer between 1 and numOfPages
+    const input = $("#currentPageInput").val();
+
+    for (let i = 0; i < input.length; i++) {
+      if (NumericalChars.includes(input[i]) === false || 1 > input || input > numOfPages) {
+        ShowError("currentPageInput", "currentPageInputErr", "Invalid page number");
+        return;
+      }
+    }
+    currentPage = parseInt(input);
+  });
+
+
+
+  function paginateTable(foundSet) {
+    numOfPages = Math.ceil(foundSet.length / resultsPerPage);
+
+    for (let i = 0; i < numOfPages; i++) {
+      for (let j = 0; j < resultsPerPage; j++) {
+
+      }
+    }
+
+    $("#showingResultsLabel").html(`Showing ${0}-${0} of ${foundSet.length}`);
+  }
+
+
+
+  // init
   searchFilter();
+  console.log("num of pages")
+  console.log(numOfPages)
 });

fce9e027f6d4fe2415c86d19c38fd5b66c38371a
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2024-04-20 18:04:38 -0400

    4/20/24 custom time interval button added

--- PMT/PMT/Services/TimeTracker/ITimeTrackerHub.cs
+++ PMT/PMT/Services/TimeTracker/ITimeTrackerHub.cs
@@ -4,12 +4,12 @@ public interface ITimeTrackerHub
 {
 	Task PrintStopwatch(int stopwatchId, string stopwatchName, bool clockIsRunning, DateTime clockRunningSince, List<TimeSetDto> timeSetDto);
 	Task PrintTimeSet(int stopwatchId, int timeSetId, string timeSetHoursText);
-	Task PrintTimeInterval(int stopwatchId, TimeIntervalDto timeIntervalDto);
+	Task PrintTimeInterval(int stopwatchId, TimeIntervalDto timeIntervalDto, int timeSetId, string timeSetMsg, double stopwatchMilli);
 	Task ClockOutTimeInterval(int stopwatchId, int timeIntervalId, string clockOut, double hours);
-	Task PauseUpdate(int stopwatchId, double miliFromServer);
+	Task PauseUpdate(int stopwatchId, double milliFromServer);
 	Task DelStopwatch(int stopwatchId);
 	Task PopulateEditIntervalModal(DateTime startDate, DateTime endDate);
-	Task EditTimeInterval(int stopwatchId, int timeSetId, int timeIntervalId, bool isFromActiveTimeSet, double stopwatchMili, string timeSetMsg, TimeIntervalDto intervalDto);
-	Task DelTimeInterval(int stopwatchId, int timeSetId, int timeIntervalId, bool isFromActiveTimeSet, bool isLastIntervalInTimeSet, string timeSetTrMsg, double stopwatchMili);
+	Task EditTimeInterval(int stopwatchId, int timeSetId, int timeIntervalId, bool isFromActiveTimeSet, double stopwatchMilli, string timeSetMsg, TimeIntervalDto intervalDto);
+	Task DelTimeInterval(int stopwatchId, int timeSetId, int timeIntervalId, bool isFromActiveTimeSet, bool isLastIntervalInTimeSet, string timeSetTrMsg, double stopwatchMilli);
   Task TimeSetRefresh(int stopwatchId, int timeSetId, string timeSetText);
 }
